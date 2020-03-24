cc6d69b31f79cf2e34aff5b5daf7552a13bd4f89
Linos Nchena <linos.nchena@solarwinds.com>
2020-03-25 00:17:05 +0100

    update

--- README.md
+++ README.md
@@ -13,3 +13,4 @@ Here are screens shots from the application UI interfaces
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'banked'`, () => {
+  it(`should have as title 'bankedx'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('banked');
+    expect(app.title).toEqual('bankedx');
   });
 
   it('should render title in a h1 tag', () => {
